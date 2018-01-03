6e1d530000197f5355d762fada04ab3f718b942b
luzhongze <luzhongze@outlook.com>
2018-01-03 20:30:58 +0800

    创建movie项目

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const Header = props => (
+  <View>
+    
+    </View>
+)
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
